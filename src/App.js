a6d89f36d74bb24b79db88d4a96040b0952bba7a
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-14 16:56:55 -0500

    fixed
    instructor fixes implemented

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 import './App.css';
-import { Greeting } from './Greeting';
-import { CongratsMsg } from './CongratsMsg';
-import { CounterButton } from './CounterButton';
-import { PersonnelList } from './PersonnelList';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// the next import possible bc used index.js in pages folder to export the pages
-// otherwise needs separate lines for each page in the folder
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { HomePage, CounterButtonPage, PersonnelListPage } from './pages';
 
-
 function App() {
-  const [timesClicked, setTimesClicked] = useState(0);
-  const increment = () => setTimesClicked(timesClicked + 1);
-  const [hideMsg, setHideMsg] = useState(false);
-  // a <Router></Router> contains all the Route objects
-  // <Route></Route> components have a path prop
-  // "home page" or desired default page prop is path="/"
-  // when url matches path, components of page displayed (like all routing)
-  // adding exact prop to a Route specifies the component will only render when
-  // the route in the url EXACTLY matches the specified path attribute value
-  // in the below case, HomePage will only render if the url ends with exactly /
-  // <PageName /> components within a specific <Route></Route> parent are displayed
-  // when the path prop value is in url
   return (
     <div className="App">
-      <Router>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/counter">
-          <CounterButtonPage />
-        </Route>
-        <Route path="/personnel-list">
-          <PersonnelListPage />
-        </Route>
-      </Router>
+      <BrowserRouter>
+        <Link to="/counter">Counter Page </Link>
+        <Link to="/personnel-list">Personnel Page</Link>
+        <Routes>
+          <Route exact path="/" element={ <HomePage /> } />
+          <Route exact path="/counter" element={ <CounterButtonPage /> } />
+          <Route exact path="/personnel-list" element={ <PersonnelListPage /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
