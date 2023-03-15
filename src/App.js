d9b39f071483eea797a879cd72a0fc45a974902d
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-14 20:33:00 -0500

    following instructor...
    prof wants to use "query-string"
    linked page in class site says use:
    npm install query-string
    so I did that... now it's broken...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { HomePage, CounterButtonPage, PersonnelListPage } from './pages';
 
+// import link from react-dom. using anchor <a></a> reloads the app when switching pages
+// url and query parameters are info about the page contained in in the url
+// url parameters: segments of url path such as .com/this/that/
+// query parameters: after ? at end of url .com/search?q=this&that_thing
+// can copy the url to replicate a search
+// adding /: to the path allows using segments of url as url params
 function App() {
   return (
     <div className="App">
@@ -10,7 +16,7 @@ function App() {
         <Link to="/personnel-list">Personnel Page</Link>
         <Routes>
           <Route exact path="/" element={ <HomePage /> } />
-          <Route exact path="/counter" element={ <CounterButtonPage /> } />
+          <Route exact path="/counter/:name" element={ <CounterButtonPage /> } />
           <Route exact path="/personnel-list" element={ <PersonnelListPage /> } />
         </Routes>
       </BrowserRouter>
--- src/pages/CounterButtonPageMk1.js
+++ src/pages/CounterButtonPageMk1.js
@@ -1,24 +1,19 @@
 import { CounterButton } from '../CounterButton';
 import { CongratsMsg } from '../CongratsMsg';
 import { useState } from 'react';
-// to use url parameters, import the following v(ersion corrections by prof)
-import { useParams, useLocation } from 'react-router-dom';
-import queryStirng from 'query-string';
-// note: must npm install query-string
+// to use url parameters, import the following, then define one in route
+import { useParams } from 'react-router-dom';
 
 export const CounterButtonPage = () => {
   const [timesClicked, setTimesClicked] = useState(0);
   const increment = () => setTimesClicked(timesClicked + 1);
   const [hideMsg, setHideMsg] = useState(false);
-  
-  // final version of lesson 3.2 code here?
-  const pageLocation = useLocation();
-  const initialValue = queryString.parse(location.search).initialValue;
-
+  const { name } = useParams();
+  // gets url parameter name as passed from app page
   return (
     <>
       <h1>My Counter Button Page</h1>
-      <h3>Recognizing {name}</h3>
+      <h3>Recognizing { name }</h3>
       {hideMsg ? null 
         : <CongratsMsg timesClicked={timesClicked} 
             triggerClicks={12} 
